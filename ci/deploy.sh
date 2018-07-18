67f0ad1c75c1b5f936198f2e4de40229025e7193
Claire Griffin <clairetg@stanford.edu>
2018-07-18 13:42:41 -0700

    update deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,6 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	mvn -P travis-maven-central --settings ci/settings.xml ;
-	docker login -u $DOCKER_USERNAME -p $DOCKER_PASSPHRASE ;
   mvn -P travis-docker-hub ;
 fi
